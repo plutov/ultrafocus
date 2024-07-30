635dec0827a274679e79b785c549651f5502d35e
Rafael Pazini <rflpazini@gmail.com>
2024-07-30 00:08:52 -0300

    refactor: removes gmails host

--- hosts/domains.go
+++ hosts/domains.go
@@ -2,7 +2,7 @@ package hosts
 
 var DefaultDomains = []string{
 	// Social media
-	"facebook.com", "instagram.com", "twitter.com", "tiktok.com", "snapchat.com", "pinterest.com", "linkedin.com", "reddit.com", "imgur.com", "youtube.com", "whatsapp.com", "telegram.org", "discord.com", "gmail.com",
+	"facebook.com", "instagram.com", "twitter.com", "tiktok.com", "snapchat.com", "pinterest.com", "linkedin.com", "reddit.com", "imgur.com", "youtube.com", "whatsapp.com", "telegram.org", "discord.com",
 
 	// News
 	"bbc.com", "cnn.com", "aljazeera.com", "theguardian.com", "nytimes.com", "google.com/news", "apple.news",
